fc9497932b5cd6b853e3aa3b28d83d906f87b6a3
Ivan Larin <pentusha@gmail.com>
2022-09-13 15:27:04 +0300

    refactor(queries): remove index argument from conditions

--- edgeql_qb/render/condition.py
+++ edgeql_qb/render/condition.py
@@ -21,16 +21,9 @@ from edgeql_qb.render.types import RenderedQuery
 from edgeql_qb.types import unsafe_text
 
 
-def render_filters(
-        filters: tuple[Expression, ...],
-        generator: Iterator[int],
-) -> RenderedQuery:
+def render_filters(filters: tuple[Expression, ...], generator: Iterator[int]) -> RenderedQuery:
     conditions = [
-        render_condition(
-            filter_.to_infix_notation(index),
-            index,
-            generator,
-        )
+        render_condition(filter_.to_infix_notation(index), generator)
         for index, filter_ in enumerate(filters)
     ]
     return combine_renderers(
@@ -39,31 +32,20 @@ def render_filters(
     )
 
 
-def render_conditions(
-        filters: tuple[Expression, ...],
-        generator: Iterator[int],
-) -> RenderedQuery:
+def render_conditions(filters: tuple[Expression, ...], generator: Iterator[int]) -> RenderedQuery:
     return filters and render_filters(filters, generator) or RenderedQuery()
 
 
 @singledispatch
-def render_condition(
-        expression: AnyExpression,
-        index: int,
-        generator: Iterator[int],
-) -> RenderedQuery:
-    raise NotImplementedError(f'{expression!r} {index=} is not supported')  # pragma: no cover
+def render_condition(expression: AnyExpression, generator: Iterator[int]) -> RenderedQuery:
+    raise NotImplementedError(f'{expression!r} is not supported')  # pragma: no cover
 
 
 @render_condition.register
-def _(
-        expression: FuncInvocation,
-        index: int,
-        generator: Iterator[int],
-) -> RenderedQuery:
+def _(expression: FuncInvocation, generator: Iterator[int]) -> RenderedQuery:
     func = expression.func
     arg_renderers = [
-        render_condition(arg, index, generator)
+        render_condition(arg, generator)
         for arg in expression.args
     ]
     return combine_many_renderers(
@@ -75,45 +57,33 @@ def _(
 
 
 @render_condition.register
-def _(expression: Node, index: int, generator: Iterator[int]) -> RenderedQuery:
+def _(expression: Node, generator: Iterator[int]) -> RenderedQuery:
     if expression.right is None:
         return combine_many_renderers(
             RenderedQuery(expression.op),
-            render_condition(expression.left, index, generator),
+            render_condition(expression.left, generator),
         )
     return render_binary_node(
-        left=render_condition(expression.left, index, generator),
-        right=render_condition(expression.right, index, generator),
+        left=render_condition(expression.left, generator),
+        right=render_condition(expression.right, generator),
         expression=expression,
     )
 
 
 @render_condition.register
-def _(
-        expression: QueryLiteral,
-        index: int,
-        generator: Iterator[int],
-) -> RenderedQuery:
+def _(expression: QueryLiteral, generator: Iterator[int]) -> RenderedQuery:
     index = next(generator)
     name = f'filter_{index}'
     return render_query_literal(expression.value, name)
 
 
 @render_condition.register
-def _(
-        expression: unsafe_text,
-        index: int,
-        generator: Iterator[int],
-) -> RenderedQuery:
+def _(expression: unsafe_text, generator: Iterator[int]) -> RenderedQuery:
     return RenderedQuery(expression)
 
 
 @render_condition.register
-def _(
-        expression: Column,
-        index: int,
-        generator: Iterator[int],
-) -> RenderedQuery:
+def _(expression: Column, generator: Iterator[int]) -> RenderedQuery:
     columns = linearize_filter_left(expression)
     dot_names = '.'.join(c.column_name for c in columns)
     return RenderedQuery(f'.{dot_names}')
