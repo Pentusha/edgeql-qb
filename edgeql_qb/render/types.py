b351c5e7cc2e4d83a9a33308f7cbb3b27741ea96
Ivan Larin <pentusha@gmail.com>
2023-05-15 14:43:50 +0300

    style: ignore ruff's false-positive warning

--- edgeql_qb/render/types.py
+++ edgeql_qb/render/types.py
@@ -7,7 +7,7 @@ from edgeql_qb.frozendict import FrozenDict
 @dataclass(slots=True, frozen=True)
 class RenderedQuery:
     query: str = ''
-    context: FrozenDict = FrozenDict()
+    context: FrozenDict = FrozenDict()  # noqa: RUF009
 
     def map(self, f: Callable[['RenderedQuery'], 'RenderedQuery']) -> 'RenderedQuery':
         return f(self)
